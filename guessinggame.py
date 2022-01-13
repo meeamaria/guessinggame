c013d6efb1b80f1bc441869f1c1a7b899bab4584
meeamaria <meeavirolainen@gmail.com>
2022-01-13 13:09:27 +0200

    Editing guessinggame to have clearConsole function and making actual gameplay to it's own function

---
+++ guessinggame.py
@@ -0,0 +1,68 @@
+import random
+import os
+
+#Function for clearing console
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
+# This function explains rules and asks for name of player.
+def intro():
+  print("Welcome to the Random number guessing game!")
+  print("May I ask your name?")
+  name = input()
+  clearConsole()
+  print(f"Hello {name}! Let me explain rules to you quickly.")
+  print("I have picked number between 1-10. You get 3 guesses to get it right.")
+  print("With every wrong answer you will lose a live. If you guess right you win the game!")
+  print("Let's get started!")
+
+#This function is for gameplay. It lets player guess and tell if they win or lose
+def gameplay():
+  #Defining starting points for variables in code
+  computer_choice = random.randint(1, 10)
+  player_lives = 3
+  end_of_game = False
+  player_guesses = []
+
+  while not end_of_game:
+    if player_lives > 0:
+      guess = int(input("Guess the number! "))
+      if guess in player_guesses:
+        print(f"You already guessed {guess}. Try again!")
+      elif guess == computer_choice:
+        print(f"You won game with {player_lives} live(s) remaining.")
+        end_of_game = True
+      elif guess > computer_choice:
+        player_lives -= 1
+        player_guesses.append(guess)
+        print(f"{guess} is too high! You have {player_lives} live(s) remaining.")
+      else:
+        player_lives -= 1
+        player_guesses.append(guess)
+        print(f"{guess} is too low. You have {player_lives} live(s) remaining.")
+    else:
+      end_of_game = True
+      print("You lost!")
+
+def playingagain():
+  print("Do you want to play again? Yes/y/No/n")
+  playagain = input() 
+
+
+playagain = True
+
+intro()
+gameplay()
+playingagain()
+
+if playagain.lower() == "yes" or "y":
+  clearConsole()
+  intro()
+  gameplay()
+  playingagain()
+else:
+  print("Thank you for playing! See you next time!")
+    
